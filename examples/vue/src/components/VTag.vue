54d74fd130681f1b6bdc0abaf3d9764e2a7f87b2
nikaplezhou <nikaplezhou@tencent.com>
2019-12-08 18:34:28 +0800

    chore: add vue example from https://github.com/gothinkster/vue-realworld-example-app

---
+++ examples/vue/src/components/VTag.vue
@@ -0,0 +1,22 @@
+<template>
+  <router-link :to="homeRoute" :class="className" v-text="name"></router-link>
+</template>
+
+<script>
+export default {
+  name: "RwvTag",
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    className: {
+      type: String,
+      default: "tag-pill tag-default"
+    }
+  },
+  computed: {
+    homeRoute: () => ({ name: "home-tag", params: { tag: name } })
+  }
+};
+</script>
