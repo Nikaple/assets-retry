40c104667172bd24cdb590b7a8dfaf2ccae5ad34
nikaplezhou <nikaplezhou@tencent.com>
2019-12-25 01:02:55 +0800

    fix: fix onload callback did not fire when resource not in domain list

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,9 @@ module.exports = {
         '.(ts|tsx)': 'ts-jest'
     },
     testEnvironment: 'jsdom',
+    testEnvironmentOptions: {
+        resources: "usable"
+    },
     testRegex: 'test/[A-Za-z-_]+\\.test\\.(ts|tsx|js)$',
     moduleFileExtensions: ['ts', 'tsx', 'js'],
     coveragePathIgnorePatterns: ['/node_modules/', '/test/'],
